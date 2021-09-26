844051f48fdce361e75a798ad227f332b1becf0f
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 18:50:38 +0530

    Added: to print line length

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,15 @@
 package linecomparison;
 
 public class LineComparison {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Line comparison Computation program");
+		double length(int x1,int y1,int x2,int y2) {
+			double lineDistance;
+			lineDistance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+			return lineDistance;
+		}
+			public static void main(String[] args) {
+		LineComparison line = new LineComparison();
+		double result = line.length(1, 9, 9, 7);
+		System.out.println("Length Of The Line:" +result);
 	}
 }
+
