73c8335c457d91e245d0ef64a72d144f56b2e9a2
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 22:18:34 +0530

    Added: to check equality of two line

--- LineComparison.java
+++ LineComparison.java
@@ -1,15 +1,18 @@
 package linecomparison;
 
 public class LineComparison {
-		double length(int x1,int y1,int x2,int y2) {
-			double lineDistance;
-			lineDistance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-			return lineDistance;
+	double length(int x1,int y1,int x2,int y2) {
+		double lineDistance;
+		lineDistance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return lineDistance;
 		}
-			public static void main(String[] args) {
-		LineComparison line = new LineComparison();
-		double result = line.length(1, 9, 9, 7);
-		System.out.println("Length Of The Line:" +result);
+		public static void main(String[] args) {
+			LineComparison line = new LineComparison();
+			Double line1= line.length(1, 7, 2, 1);
+			Double line2= line.length(9, 7, 9, 8);
+				System.out.println("The Two Lines Are Equal:" +line1.equals(line2));
 	}
 }
+
+
 
