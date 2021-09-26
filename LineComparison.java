4d5160df041430de38becd0077bbe1dde61c0714
prasadv1998 <prasadan1720@gmail.com>
2021-09-26 22:27:58 +0530

    Added: to compare two lines

--- LineComparison.java
+++ LineComparison.java
@@ -2,15 +2,20 @@ package linecomparison;
 
 public class LineComparison {
 	double length(int x1,int y1,int x2,int y2) {
-		double lineDistance;
-		lineDistance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		return lineDistance;
+	double lineDistance;
+	lineDistance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+	return lineDistance;	}
+	public static void main(String[] args) {
+		LineComparison line = new LineComparison();
+		Double line1= line.length(1, 7, 2, 1);
+		Double line2= line.length(9, 7, 9, 8);
+		if(Double.compare(line1,line2) == 0) {
+			System.out.println("Two Results Are Equal");
+		}else if(Double.compare(line1,line2) < 0) {
+			System.out.println("Line1 Is Less Than Line2");
+		}else {
+			System.out.println("Line1 Is Greater Than Line2");
 		}
-		public static void main(String[] args) {
-			LineComparison line = new LineComparison();
-			Double line1= line.length(1, 7, 2, 1);
-			Double line2= line.length(9, 7, 9, 8);
-				System.out.println("The Two Lines Are Equal:" +line1.equals(line2));
 	}
 }
 
